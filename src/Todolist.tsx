846f0c5620797ea23737eb1e0cfb0fc5d43bc755
Alik_Avizha <aleksandravizha@icloud.com>
2023-02-27 22:32:48 +0300

    work with components

---
+++ src/Todolist.tsx
@@ -0,0 +1,40 @@
+import React, {FC} from 'react';
+import {TaskType} from "./App";
+
+
+type TodolistPropsType = {
+    title: string,
+    tasks: Array<TaskType>
+}
+
+
+const Todolist: FC<TodolistPropsType> = (props) => {
+    let isAllTasksNotIsDone = true
+    for (let i = 0; i < props.tasks.length; i++) {
+        if(props.tasks[i].isDone){
+            isAllTasksNotIsDone = false
+            break;
+        }
+    }
+    const todoClasses = isAllTasksNotIsDone ? "todolist-empty" : "todolist"
+    return (
+        <div className={todoClasses}>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+export default Todolist;
